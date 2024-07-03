009594329aec7ecbeb258f294f21d87b2f828b00
RohitAsundi <91424033+RohitAsundi@users.noreply.github.com>
2024-07-03 10:49:47 +0530

    Delete userservice directory

--- userservice/userservice/src/main/java/com/ascent/userservice/Exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ascent.userservice.Exceptions;
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(){
-       super("Resource Not Found...");
-    }
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
